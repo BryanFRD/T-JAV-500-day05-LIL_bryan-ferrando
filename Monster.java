4bb0babc29b8d850d8525b805095187353c31d95
Bryan <bryan.ferrando@epitech.eu>
2023-10-27 08:49:55 +0100

    Fixes

---
+++ Monster.java
@@ -0,0 +1,42 @@
+public abstract class Monster extends Unit {
+
+    protected int damage = 0;
+    protected int apcost = 0;
+
+    protected Monster(String name, int hp, int ap) {
+        super(name, hp, ap);
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public int getApcost(){
+        return apcost;
+    }
+
+    @Override
+    public boolean equip(Weapon weapon) {
+        System.out.println("Monsters are proud and fight with their own bodies.");
+        return false;
+    }
+
+    @Override
+    public boolean attack(Fighter fighter) {
+        if (fighter == null) {
+            return false;
+        }
+        if (closeTo != fighter) {
+            System.out.println(getName() + ": I'm too far away from " + fighter.getName());
+            return false;
+        }
+        if (ap < apcost) {
+            return false;
+        }
+        ap -= apcost;
+        System.out.println(getName() + " attacks " + fighter.getName());
+        fighter.receiveDamage(damage);
+        return true;
+    }
+
+}
