2742cff811ea8fe0671d27d5ff191b9832d363d1
Bryan <bryan.ferrando@epitech.eu>
2023-10-27 10:11:54 +0100

    Fixes

---
+++ SuperMutant.java
@@ -0,0 +1,16 @@
+public class SuperMutant extends Monster {
+
+    private static int id = 0;
+
+    protected SuperMutant() {
+        super("SuperMutant #" + ++id, 170, 20);
+        damage = 60;
+    }
+
+    @Override
+    public void recoverAP() {
+        super.recoverAP();
+        hp = Math.min(170, hp + 10);
+    }
+
+}
